dbb33b91b3b407498aa9b9ba7e3f8bb0cc839bfe
MacielDev <andreraizdetudo@gmail.com>
2022-08-24 17:17:49 -0300

    Criação da página 404

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import './assets/css/base/base.css'
+import { Route, Routes } from 'react-router-dom'
+
+import Home from './paginas/Home';
+import Sobre from './paginas/Sobre';
+import Pagina404 from './paginas/Pagina404';
+
 
 function App() {
+  const rotas = () => {
+    return (
+     <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/sobre" element={<Sobre/>} />
+      <Route path="/*" element={<Pagina404/>} />
+     </Routes>
+    )
+  }
   return (
   <>
-
+  {rotas()}
   </>
   )
 }
 
-export default App
+export default App;
